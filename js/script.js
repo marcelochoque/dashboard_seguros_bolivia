20878affdbe39f365255bdecd934dc51465f034e
Marcelo Choque <ing.marcelo.choque@gmail.com>
2025-02-09 22:48:47 -0400

    arreglo direcciones

--- js/script.js
+++ js/script.js
@@ -479,7 +479,7 @@ function convertCumulativeToMonthly(data) {
 }
 
 // --- CARGA DEL CSV CON Papa Parse ---
-Papa.parse("data/data_insurance_2024.csv", {
+Papa.parse("https://marcelochoque.github.io/dashboard_seguros_bolivia/data/data_insurance_2024.csv", {
   download: true,
   header: true,
   dynamicTyping: true,
